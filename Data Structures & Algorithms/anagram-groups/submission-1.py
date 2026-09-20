a5fa6059af3e8b7353f9ec73f5b0d0d9454f2857
Chris Wallerstein <t@cmgwallerstein.net>
2026-09-20 10:06:52 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+def get_key(s: str) -> tuple[int]:
+    char_list = [0] * 26
+    for i in [ord(c) % 26 for c in s]:
+        char_list[i] += 1
+    return tuple(char_list)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for s in strs:
+            key = get_key(s)
+            anagrams[key].append(s)
+        return [anagrams[i] for i in anagrams]
+            
